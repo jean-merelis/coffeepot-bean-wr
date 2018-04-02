6797aa214e251c0e5ee40b42e2bc6957e3527cc4
Jeandeson O Merelis <jean.merelis@gmail.com>
2018-04-02 14:30:11 -0300

    added prefix and suffix commands to DefaultStringHandler

--- src/main/java/coffeepot/bean/wr/typeHandler/DefaultStringHandler.java
+++ src/main/java/coffeepot/bean/wr/typeHandler/DefaultStringHandler.java
@@ -12,9 +12,9 @@ package coffeepot.bean.wr.typeHandler;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -70,7 +70,6 @@ import java.util.List;
  * --onlyOnWrite).<br>
  * This command use the 'replaceAll' method of the String class.
  * <code>s.replaceAll({arg0}, {arg1});</code></p>
- * <p>
  *
  * <p>
  * <strong>CharCase command</strong>: "charCase".<br>
@@ -82,24 +81,93 @@ import java.util.List;
  */
 public class DefaultStringHandler implements TypeHandler<String> {
 
+    /**
+     * Replace command: "replace".
+     * <p>
+     * Replace command require two args, the first arg is the target and the
+     * second is the replacement. A third optional argument can be provided to
+     * determine whether command is read-only or write-only (--onlyOnRead or
+     * --onlyOnWrite).<br>
+     * This command use the 'replace' method of the String class.
+     * <code>s.replace({arg0}, {arg1});</code></p>
+     * <p>
+     */
     public static final String CMD_REPLACE = "replace";
+
+    /**
+     * Replace First command: "replaceFirst".
+     * <p>
+     * ReplaceFirst command require two args, the first arg is a regex target
+     * and the second is the replacement. A third optional argument can be
+     * provided to determine whether command is read-only or write-only
+     * (--onlyOnRead or --onlyOnWrite).<br>
+     * This command use the 'replaceFirst' method of the String class.
+     * <code>s.replaceFirst({arg0}, {arg1});</code></p>
+     * <p>
+     */
     public static final String CMD_REPLACE_FIRST = "replaceFirst";
+
+    /**
+     * Replace All command: "replaceAll".
+     * <p>
+     * ReplaceAll command require two args, the first arg is a regex target and
+     * the second is the replacement. A third optional argument can be provided
+     * to determine whether command is read-only or write-only (--onlyOnRead or
+     * --onlyOnWrite).<br>
+     * This command use the 'replaceAll' method of the String class.
+     * <code>s.replaceAll({arg0}, {arg1});</code></p>
+     */
     public static final String CMD_REPLACE_ALL = "replaceAll";
+
+    /**
+     * Add prefix command: "addPrefix".
+     * <p>
+     * The addPrefix command require one arg. The first arg is used to prefix
+     * the text. A second optional argument can be provided to determine whether
+     * command is read-only or write-only (--onlyOnRead or --onlyOnWrite).
+     */
+    public static final String CMD_ADD_PREFIX = "addPrefix";
+
+    /**
+     * Add suffix command: "addSuffix".
+     * <p>
+     * The addSuffix command require one arg. The first arg is used to suffix
+     * the text. A second optional argument can be provided to determine whether
+     * command is read-only or write-only (--onlyOnRead or --onlyOnWrite).
+     * <strong>This command is applyed</strong>
+     *
+     */
+    public static final String CMD_ADD_SUFFIX = "addSuffix";
 
     public static final String OPT_ONLY_ON_READ = "--onlyOnRead";
     public static final String OPT_ONLY_ON_WRITE = "--onlyOnWrite";
 
+    /**
+     * Filter command: "filter".
+     * <p>
+     * The Filter command require one arg. The first arg is a regex target to be
+     * used to filter the string. A second optional argument can be provided to
+     * determine whether command is read-only or write-only (--onlyOnRead or
+     * --onlyOnWrite).<br>
+     * This command use the 'replaceAll' method of the String class.
+     * <code>s.replaceAll({arg0}, "");</code></p>
+     */
     public static final String CMD_FILTER = "filter";
     public static final String FILTER_NUMBER_ONLY = "\\D";
     public static final String FILTER_NUMBER_LETTERS_ONLY = "[^0-9\\p{L}]";
 
+    /**
+     * Filter command: "filter".
+     * <p>
+     * CharCase command require one arg. The options are: "UPPER", "LOW",
+     * "NORMAL".</p>
+     */
     public static final String CMD_CHARCASE = "charCase";
     public static final String CHARCASE_UPPER = "UPPER";
     public static final String CHARCASE_LOW = "LOW";
     public static final String CHARCASE_NORMAL = "NORMAL";
 
-    private CharCase charCase;
-    private List<Replace> replaces;
+    private List<InternalCommnand> commands;
 
     public DefaultStringHandler() {
         setDefault();
@@ -112,19 +180,9 @@ public class DefaultStringHandler implements TypeHandler<String> {
             return null;
         }
 
-        switch (charCase) {
-            case UPPER:
-                text = text.toUpperCase();
-                break;
-            case LOW:
-                text = text.toLowerCase();
-                break;
-        }
-
-
-        if (replaces != null) {
-            for (Replace r : replaces) {
-                text = r.replaceOnRead(text);
+        if (commands != null) {
+            for (InternalCommnand r : commands) {
+                text = r.execOnRead(text);
             }
         }
 
@@ -137,18 +195,10 @@ public class DefaultStringHandler implements TypeHandler<String> {
             return null;
         }
 
-        switch (charCase) {
-            case UPPER:
-                text = text.toUpperCase();
-                break;
-            case LOW:
-                text = text.toLowerCase();
-                break;
-        }
 
-        if (replaces != null) {
-            for (Replace r : replaces) {
-                text = r.replaceOnWrite(text);
+        if (commands != null) {
+            for (InternalCommnand r : commands) {
+                text = r.execOnWrite(text);
             }
         }
 
@@ -156,82 +206,119 @@ public class DefaultStringHandler implements TypeHandler<String> {
     }
 
     @Override
-    public void config(Command[] commands) {
-        if (commands == null || commands.length == 0) {
+    public void config(Command[] cmds) {
+        if (cmds == null || cmds.length == 0) {
             setDefault();
             return;
         }
-        for (Command cmd : commands) {
+        for (Command cmd : cmds) {
             switch (cmd.getName()) {
                 case CMD_REPLACE: {
                     if (cmd.getArgs() == null || cmd.getArgs().length < 2) {
                         throw new IllegalArgumentException("The replace command requires two args, the third arg is optional. The first is the target and the second is the replacement");
                     }
-                    if (replaces == null) {
-                        replaces = new ArrayList<>();
+                    if (this.commands == null) {
+                        this.commands = new ArrayList<>();
                     }
 
-                    Replace r = new Replace();
-                    r.type = ReplaceType.REPLACE;
-                    r.target = cmd.getArgs()[0];
-                    r.replacement = cmd.getArgs()[1];
+                    InternalCommnand r = new InternalCommnand();
+                    r.type = CommandType.REPLACE;
+                    r.arg0 = cmd.getArgs()[0];
+                    r.arg1 = cmd.getArgs()[1];
                     defReadWriteOnly(r, cmd.getArgs().length > 2 ? cmd.getArgs()[2] : null);
-                    replaces.add(r);
+                    this.commands.add(r);
                     break;
                 }
                 case CMD_REPLACE_FIRST: {
                     if (cmd.getArgs() == null || cmd.getArgs().length < 2) {
                         throw new IllegalArgumentException("The replaceFirst command requires two args, the third arg is optional. The first is a regex and the second is the replacement");
                     }
-                    if (replaces == null) {
-                        replaces = new ArrayList<>();
+                    if (this.commands == null) {
+                        this.commands = new ArrayList<>();
                     }
 
-                    Replace r = new Replace();
-                    r.type = ReplaceType.REPLACE_FIRST;
-                    r.target = cmd.getArgs()[0];
-                    r.replacement = cmd.getArgs()[1];
+                    InternalCommnand r = new InternalCommnand();
+                    r.type = CommandType.REPLACE_FIRST;
+                    r.arg0 = cmd.getArgs()[0];
+                    r.arg1 = cmd.getArgs()[1];
                     defReadWriteOnly(r, cmd.getArgs().length > 2 ? cmd.getArgs()[2] : null);
-                    replaces.add(r);
+                    this.commands.add(r);
                     break;
                 }
                 case CMD_REPLACE_ALL: {
                     if (cmd.getArgs() == null || cmd.getArgs().length < 2) {
                         throw new IllegalArgumentException("The replaceAll command requires two args, the third arg is optional. The first is a regex and the second is the replacement");
                     }
-                    if (replaces == null) {
-                        replaces = new ArrayList<>();
+                    if (this.commands == null) {
+                        this.commands = new ArrayList<>();
                     }
 
-                    Replace r = new Replace();
-                    r.type = ReplaceType.REPLACE_ALL;
-                    r.target = cmd.getArgs()[0];
-                    r.replacement = cmd.getArgs()[1];
+                    InternalCommnand r = new InternalCommnand();
+                    r.type = CommandType.REPLACE_ALL;
+                    r.arg0 = cmd.getArgs()[0];
+                    r.arg1 = cmd.getArgs()[1];
                     defReadWriteOnly(r, cmd.getArgs().length > 2 ? cmd.getArgs()[2] : null);
-                    replaces.add(r);
+                    this.commands.add(r);
                     break;
                 }
                 case CMD_FILTER: {
                     if (cmd.getArgs() == null || cmd.getArgs().length == 0) {
                         throw new IllegalArgumentException("The filter command requires one arg, the second arg is optional. The first is a regex used to filter the string");
                     }
-                    if (replaces == null) {
-                        replaces = new ArrayList<>();
+                    if (this.commands == null) {
+                        this.commands = new ArrayList<>();
                     }
 
-                    Replace r = new Replace();
-                    r.type = ReplaceType.REPLACE_ALL;
-                    r.target = cmd.getArgs()[0];
-                    r.replacement = "";
+                    InternalCommnand r = new InternalCommnand();
+                    r.type = CommandType.REPLACE_ALL;
+                    r.arg0 = cmd.getArgs()[0];
+                    r.arg1 = "";
                     defReadWriteOnly(r, cmd.getArgs().length > 1 ? cmd.getArgs()[1] : null);
-                    replaces.add(r);
+                    this.commands.add(r);
                     break;
                 }
                 case CMD_CHARCASE: {
                     if (cmd.getArgs() == null || cmd.getArgs().length == 0) {
                         throw new IllegalArgumentException("The charCase command requires one arg. The first is a regex used to filter the string");
                     }
-                    charCase = CharCase.valueOf(cmd.getArgs()[0]);
+                    if (this.commands == null) {
+                        this.commands = new ArrayList<>();
+                    }
+                    InternalCommnand r = new InternalCommnand();
+                    r.type = CommandType.CHARCASE;
+                    r.charCase = CharCase.valueOf(cmd.getArgs()[0]);
+                    defReadWriteOnly(r, cmd.getArgs().length > 1 ? cmd.getArgs()[1] : null);
+                    this.commands.add(r);
+                    break;
+                }
+                case CMD_ADD_PREFIX: {
+                    if (cmd.getArgs() == null || cmd.getArgs().length == 0) {
+                        throw new IllegalArgumentException("The addPrefix command requires one arg, the second arg is optional.");
+                    }
+                    if (this.commands == null) {
+                        this.commands = new ArrayList<>();
+                    }
+
+                    InternalCommnand r = new InternalCommnand();
+                    r.type = CommandType.ADD_PREFIX;
+                    r.arg0 = cmd.getArgs()[0];
+                    defReadWriteOnly(r, cmd.getArgs().length > 1 ? cmd.getArgs()[1] : null);
+                    this.commands.add(r);
+                    break;
+                }
+                case CMD_ADD_SUFFIX: {
+                    if (cmd.getArgs() == null || cmd.getArgs().length == 0) {
+                        throw new IllegalArgumentException("The addSuffix command requires one arg, the second arg is optional.");
+                    }
+                    if (this.commands == null) {
+                        this.commands = new ArrayList<>();
+                    }
+
+                    InternalCommnand r = new InternalCommnand();
+                    r.type = CommandType.ADD_SUFFIX;
+                    r.arg0 = cmd.getArgs()[0];
+                    defReadWriteOnly(r, cmd.getArgs().length > 1 ? cmd.getArgs()[1] : null);
+                    this.commands.add(r);
                     break;
                 }
                 default: {
@@ -241,7 +328,7 @@ public class DefaultStringHandler implements TypeHandler<String> {
         }
     }
 
-    private void defReadWriteOnly(Replace r, String arg) {
+    private void defReadWriteOnly(InternalCommnand r, String arg) {
         r.onRead = true;
         r.onWrite = true;
 
@@ -257,26 +344,26 @@ public class DefaultStringHandler implements TypeHandler<String> {
     }
 
     private void setDefault() {
-        charCase = CharCase.NORMAL;
-        replaces = null;
+        commands = null;
     }
 
-    private static class Replace {
+    private static class InternalCommnand {
 
-        ReplaceType type;
-        String target;
-        String replacement;
+        CommandType type;
+        String arg0;
+        String arg1;
+        CharCase charCase;
         boolean onRead;
         boolean onWrite;
 
-        String replaceOnRead(String text) {
+        String execOnRead(String text) {
             if (!onRead) {
                 return text;
             }
             return perform(text);
         }
 
-        String replaceOnWrite(String text) {
+        String execOnWrite(String text) {
             if (!onWrite) {
                 return text;
             }
@@ -286,22 +373,44 @@ public class DefaultStringHandler implements TypeHandler<String> {
         private String perform(String text) {
             switch (type) {
                 case REPLACE:
-                    text = text.replace(target, replacement);
+                    text = text.replace(arg0, arg1);
                     break;
                 case REPLACE_FIRST:
-                    text = text.replaceFirst(target, replacement);
+                    text = text.replaceFirst(arg0, arg1);
                     break;
                 case REPLACE_ALL:
-                    text = text.replaceAll(target, replacement);
+                    text = text.replaceAll(arg0, arg1);
                     break;
+                case CHARCASE: {
+                    switch (charCase) {
+                        case UPPER:
+                            text = text.toUpperCase();
+                            break;
+                        case LOW:
+                            text = text.toLowerCase();
+                            break;
+                    }
+                    break;
+                }
+                case ADD_PREFIX: {
+                    text = arg0 + text;
+                    break;
+                }
+                case ADD_SUFFIX: {
+                    text = text + arg0;
+                    break;
+                }
             }
             return text;
         }
     }
 
-    private enum ReplaceType {
+    private enum CommandType {
         REPLACE,
         REPLACE_FIRST,
-        REPLACE_ALL;
+        REPLACE_ALL,
+        CHARCASE,
+        ADD_PREFIX,
+        ADD_SUFFIX;
     }
 }
